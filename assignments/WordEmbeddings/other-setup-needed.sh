f6d795d3998c883247a08080b70e57574fc03e27
Sofia Serrano <38511636+serrano-s@users.noreply.github.com>
2025-09-21 15:10:59 -0400

    Condensed other-setup-needed.sh

--- assignments/WordEmbeddings/other-setup-needed.sh
+++ assignments/WordEmbeddings/other-setup-needed.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/serrano-s/NLPassignments-students/refs/heads/main/assignments/WordEmbeddings/wordvec_tests.py
+wget $(python data_path_to_download_url.py "assignments/WordEmbeddings/wordvec_tests.py")
 glovepartone="data/embeddings/glove.6B/glove.6B.50d.part1.txt"
 gloveparttwo="data/embeddings/glove.6B/glove.6B.50d.part2.txt"
 overallglove="data/embeddings/glove.6B/glove.6B.50d.txt"
