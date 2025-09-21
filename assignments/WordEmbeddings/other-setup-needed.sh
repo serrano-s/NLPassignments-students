346728da5cd1b49c77b34c11949651be6a5f3372
Sofia Serrano <38511636+serrano-s@users.noreply.github.com>
2025-09-21 15:19:11 -0400

    Update other-setup-needed.sh

--- assignments/WordEmbeddings/other-setup-needed.sh
+++ assignments/WordEmbeddings/other-setup-needed.sh
@@ -1,4 +1,5 @@
 wget $(python data_path_to_download_url.py "assignments/WordEmbeddings/wordvec_tests.py")
+wget $(python data_path_to_download_url.py "assignments/WordEmbeddings/glove.py")
 glovepartone="data/embeddings/glove.6B/glove.6B.50d.part1.txt"
 gloveparttwo="data/embeddings/glove.6B/glove.6B.50d.part2.txt"
 overallglove="data/embeddings/glove.6B/glove.6B.50d.txt"
