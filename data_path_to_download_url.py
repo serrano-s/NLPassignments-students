135b77925ef6be55731104f24912ed0e149c49e1
serrano-s <sofia.elisa.serrano@gmail.com>
2025-09-21 14:46:16 -0400

    Modifying data_path_to_download

--- data_path_to_download_url.py
+++ data_path_to_download_url.py
@@ -1,3 +1,3 @@
 from sys import argv
 desired_data_path = argv[1]
-print("https://github.com/serrano-s/NLPassignments-students/blob/main/" + desired_data_path.strip())
+print("https://raw.githubusercontent.com/serrano-s/NLPassignments-students/refs/heads/main/" + desired_data_path.strip())
